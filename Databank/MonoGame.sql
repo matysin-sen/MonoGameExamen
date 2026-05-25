30740b6e587f21003d72c098a7ab76ef800aafb2
matysin-sen <238651134+matysin-sen@users.noreply.github.com>
2026-05-25 16:34:59 +0200

    databank imported

---
+++ Databank/MonoGame.sql
@@ -0,0 +1,9 @@
+create database MonoGameHighScore;
+
+create table highScoreMultiplayer(
+id int Identity(1,1),
+score int);
+
+create table highScoreSingleplayer(
+id int Identity(1,1),
+score int);
